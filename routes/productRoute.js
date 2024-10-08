54960f9b26674ffec6c7a46f9715cd429a637797
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-09 03:27:18 +0600

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,15 @@
 import express from "express"
 import  {addProduct,listProducts,removeProducts,singleProductInfo} from '../controllers/productController.js'
+import upload from "../middleware/multer.js"
 
 const productRouter = express.Router()
 
-productRouter.post('/add',addProduct)
+productRouter.post('/add',upload.fields([{name:'image1', maxCount:1},{name:'image2', maxCount:1},{name:'image3', maxCount:1},{name:'image4', maxCount:1}]),addProduct)
+//the upload.fields() runs first. It intercepts the request, processes the file uploads (i.e., stores them), and attaches the uploaded files to req.files.
+
+//addProduct: After the middleware completes, the addProduct controller runs. 
+//Inside the controller, you can access the uploaded files through req.files to perform actions like saving the file paths to the database.
+
 productRouter.post('/remove',removeProducts)
 productRouter.post('/single',singleProductInfo)
 productRouter.get('/list',listProducts)
