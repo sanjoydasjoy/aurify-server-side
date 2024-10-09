d5ba04994677f5c2dcdf07ba69391219f784dbef
SANJOY DAS <106918069+sanjoydasjoy@users.noreply.github.com>
2024-10-09 19:07:55 +0600

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -44,7 +44,7 @@ const addProduct = async (req, res) => {
 
         res.json({ success: true, message: "Product Added" })
 
-    } catch {
+    } catch(error) {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
